01139276211982a514a5011d87fb5fde65faaca3
Rafostar <40623528+Rafostar@users.noreply.github.com>
2019-01-02 13:32:04 +0100

    Improved controls when playing on smartphone

--- webplayer/webplayer.js
+++ webplayer/webplayer.js
@@ -1,12 +1,20 @@
 const isMobile = (/Android|iPhone|iPad|iPod|BlackBerry|IEMobile|Windows Phone/i.test(navigator.userAgent)) ? true : false;
 const player = new Plyr('#player', playerOptions);
+var enteredFullscreen;
 
 function startPlayer()
 {
 	/* When on mobile */ 
 	if(isMobile)
 	{
-		/* Enter fullscreen after touch */
-		player.fullscreen.enter();
+		/* Enter fullscreen after touch (only once) */
+		if(!enteredFullscreen)
+		{
+			player.fullscreen.enter();
+			enteredFullscreen = true;
+		}
+
+		/* Play and pause on touch */
+		player.togglePlay();
 	}
 }
