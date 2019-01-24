7e13e480926f46b3b3eae285de86375573d2f6ee
Rafostar <40623528+Rafostar@users.noreply.github.com>
2019-01-24 13:30:07 +0100

    Custom ffprobe implementation

---
+++ ffprobe.js
@@ -0,0 +1,35 @@
+var JSONStream = require('JSONStream');
+var deferential = require('deferential');
+var spawn = require('child_process').spawn;
+
+module.exports = getInfo;
+
+function getInfo(filePath, opts, cb)
+{
+	var d = deferential();
+	var info;
+	var stderr;
+
+	var ffprobe = spawn(opts.path, ['-show_streams', '-show_format', '-print_format', 'json', filePath]);
+
+	ffprobe.once('close', function(code)
+	{
+		if(!code)
+		{
+			d.resolve(info);
+		}
+		else
+		{
+			d.reject(new Error("FFprobe error!"));
+		}
+	});
+
+	ffprobe.stdout
+		.pipe(JSONStream.parse())
+		.once('data', function(data)
+		{
+			info = data;
+		});
+
+	return d.nodeify(cb);
+}
