1671d19feefba81681ad89d3b08101fa3622731a
Rafostar <40623528+Rafostar@users.noreply.github.com>
2019-03-12 12:36:32 +0100

    Initial add-ons support
    In the future this will allow of developing new add-ons (extensions of an extension :-) ) that will expand Cast to TV menu by adding new features

---
+++ addons.js
@@ -0,0 +1,25 @@
+const Main = imports.ui.main;
+const AggregateMenu = Main.panel.statusArea.aggregateMenu;
+
+function findCastToTv()
+{
+	let menuItems = AggregateMenu.menu._getMenuItems();
+	let index = 0;
+
+	while(index < menuItems.length)
+	{
+		if(menuItems[index].castSubMenu) break;
+		index++;
+	}
+
+	return menuItems[index];
+}
+
+function setLastMenuItem(extMenu, item, endOffset)
+{
+	if(!endOffset) endOffset = 0;
+
+	let subMenuItems = extMenu.castSubMenu.menu._getMenuItems();
+	let lastItemIndex = subMenuItems.length - 1;
+	extMenu.castSubMenu.menu.moveMenuItem(item, lastItemIndex - endOffset);
+}
