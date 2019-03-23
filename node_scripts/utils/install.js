66d705c742eaa56aa09c35f82afe275626d52b47
Rafostar <40623528+Rafostar@users.noreply.github.com>
2019-03-23 18:42:21 +0100

    Simplify Add-ons installation and updating process

---
+++ node_scripts/utils/install.js
@@ -0,0 +1,34 @@
+var fs = require('fs');
+var path = require('path');
+var { execSync } = require('child_process');
+var extensionsPath = path.join(__dirname + '/../../..');
+
+/* Add-ons should be installed synchronously */
+var extensions = fs.readdirSync(extensionsPath);
+
+extensions.forEach(folder =>
+{
+	if(folder.includes('cast-to-tv') && folder.includes('addon'))
+	{
+		var addonFolder = path.join(extensionsPath, folder);
+		var addonName = folder.substring(11, folder.lastIndexOf('-'));
+		var isPackage = fs.existsSync(addonFolder + '/package.json');
+
+		if(isPackage)
+		{
+			var installText = `Installing: Cast to TV - ${addonName[0].toUpperCase() + addonName.slice(1)} Add-on`;
+			var textLength = installText.length;
+
+			while(textLength)
+			{
+				process.stdout.write('-');
+				textLength--;
+			}
+
+			process.stdout.write('\n');
+			console.log(installText);
+			execSync('npm install', { cwd: addonFolder, stdio: 'inherit' });
+			process.stdout.write('\n');
+		}
+	}
+});
