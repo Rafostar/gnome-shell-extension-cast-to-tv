2c5605c2a0db276abe1bf9d26d2f2cffb1e4c33e
Rafostar <40623528+Rafostar@users.noreply.github.com>
2019-05-13 18:57:25 +0200

    Single dir watcher instead of per file

--- node_scripts/bridge.js
+++ node_scripts/bridge.js
@@ -1,5 +1,6 @@
 var fs = require('fs');
 var watch = require('node-watch');
+var debug = require('debug')('bridge');
 var server = require('./server');
 var encode = require('./encode');
 var extract = require('./extract');
@@ -11,6 +12,10 @@ var socket = require('./server-socket');
 var addons = require('./addons-importer');
 var shared = require('../shared');
 var remote = require(shared.remotePath);
+
+var configTimeout;
+var playlistTimeout;
+var selectionTimeout;
 
 exports.config = require(shared.configPath);
 exports.selection = require(shared.selectionPath);
@@ -30,83 +35,122 @@ exports.setStatusFile = function(status)
 	fs.writeFileSync(shared.statusPath, JSON.stringify(statusContents, null, 1));
 }
 
-watch(shared.configPath, { delay: 100 }, (eventType, filename) =>
+watch(shared.tempDir, { delay: 0 }, (eventType, filename) =>
 {
 	if(eventType == 'update')
 	{
-		var configContents = getContents(shared.configPath);
-		if(configContents === null) return;
-
-		exports.config = configContents;
-		server.refreshConfig();
-	}
-});
-
-watch(shared.selectionPath, { delay: 250 }, (eventType, filename) =>
-{
-	if(eventType == 'update')
-	{
-		var selectionContents = getContents(shared.selectionPath);
-		if(selectionContents === null || exports.list === null) return;
-
-		exports.selection = selectionContents;
-
-		/* Close addon before selecting a new one */
-		closeAddon();
-
-		if(exports.selection.addon)
+		switch(filename)
 		{
-			exports.addon = addons(exports.selection.addon.toLowerCase());
-			if(exports.addon) exports.addon.handleSelection(exports.selection, exports.config);
-
-			remove.covers();
-			remove.file(shared.vttSubsPath);
-		}
-		else if(exports.selection.filePath)
-		{
-			setProcesses();
-		}
-
-		if(exports.selection.filePath)
-		{
-			if(exports.config.receiverType == 'chromecast') chromecast.cast();
-			else if(exports.config.receiverType == 'other') setTimeout(socket.emit, 250, 'reload');
-		}
-	}
-});
-
-watch(shared.listPath, { delay: 200 }, (eventType, filename) =>
-{
-	if(eventType == 'update')
-	{
-		exports.list = getContents(shared.listPath);
-		gnome.showRemote(false);
-	}
-});
-
-watch(shared.remotePath, { delay: 0 }, (eventType, filename) =>
-{
-	if(eventType == 'update')
-	{
-		var remoteContents = getContents(shared.remotePath);
-		if(remoteContents === null) return;
-
-		remote = remoteContents;
-
-		var action = remote.action;
-		var value = remote.value;
-
-		switch(exports.config.receiverType)
-		{
-			case 'other':
-				controller.webControl(action, value);
+			case shared.configPath:
+				if(configTimeout) clearTimeout(configTimeout);
+				configTimeout = setTimeout(() =>
+				{
+					updateConfig();
+					configTimeout = null;
+				}, 250);
+				break;
+			case shared.listPath:
+				if(playlistTimeout) clearTimeout(playlistTimeout);
+				playlistTimeout = setTimeout(() =>
+				{
+					updatePlaylist();
+					playlistTimeout = null;
+				}, 100);
+				break;
+			case shared.selectionPath:
+				if(selectionTimeout) clearTimeout(selectionTimeout);
+				selectionTimeout = setTimeout(() =>
+				{
+					updateSelection();
+					selectionTimeout = null;
+				}, 150);
+				break;
+			case shared.remotePath:
+				updateRemote();
 				break;
 			default:
-				chromecast.remote(action, value);
 				break;
 		}
 	}
 });
+
+function updateConfig()
+{
+	var configContents = getContents(shared.configPath);
+	if(configContents === null) return;
+
+	exports.config = configContents;
+	debug(`New config contents: ${JSON.stringify(exports.config)}`);
+	server.refreshConfig();
+}
+
+function updatePlaylist()
+{
+	exports.list = getContents(shared.listPath);
+	if(exports.list) debug(`New playlist contents: ${JSON.stringify(exports.list)}`);
+
+	gnome.showRemote(false);
+}
+
+function updateSelection()
+{
+	/* Prevent from updating selection while playlist is still read */
+	if(playlistTimeout)
+	{
+		setTimeout(() => updateSelection(), 150);
+		return;
+	}
+
+	var selectionContents = getContents(shared.selectionPath);
+	if(selectionContents === null || exports.list === null) return;
+
+	exports.selection = selectionContents;
+	debug(`New selection contents: ${JSON.stringify(exports.selection)}`);
+
+	/* Close addon before selecting a new one */
+	closeAddon();
+
+	if(exports.selection.addon)
+	{
+		exports.addon = addons(exports.selection.addon.toLowerCase());
+		if(exports.addon) exports.addon.handleSelection(exports.selection, exports.config);
+
+		remove.covers();
+		remove.file(shared.vttSubsPath);
+	}
+	else if(exports.selection.filePath)
+	{
+		setProcesses();
+	}
+
+	if(exports.selection.filePath)
+	{
+		if(exports.config.receiverType == 'chromecast') chromecast.cast();
+		else if(exports.config.receiverType == 'other') setTimeout(socket.emit, 250, 'reload');
+	}
+}
+
+function updateRemote()
+{
+	var remoteContents = getContents(shared.remotePath);
+	if(remoteContents === null) return;
+
+	debug(`New remote contents: ${JSON.stringify(remoteContents)}`);
+	remote = remoteContents;
+
+	var action = remote.action;
+	var value = remote.value;
+
+	switch(exports.config.receiverType)
+	{
+		case 'other':
+			controller.webControl(action, value);
+			break;
+		default:
+			chromecast.remote(action, value);
+			break;
+	}
+}
 
 function getContents(path)
 {
@@ -115,7 +159,12 @@ function getContents(path)
 	delete require.cache[path];
 
 	try { data = require(path); }
-	catch(err) { data = null }
+	catch(err)
+	{
+		debug(`Could not read file: ${path}`);
+		debug(err);
+		data = null
+	}
 
 	return data;
 }
