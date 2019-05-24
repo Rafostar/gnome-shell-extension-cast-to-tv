4efadc8a7955f3184b617e5c969018fbe908cb4a
Rafostar <40623528+Rafostar@users.noreply.github.com>
2019-05-24 19:57:50 +0200

    Fixed wrong browser image due to cache

--- webplayer/webplayer-init.js
+++ webplayer/webplayer-init.js
@@ -53,19 +53,6 @@ function addClickListeners()
 	div.addEventListener('click', startPlayer);
 }
 
-function makeID()
-{
-	var text = "";
-	var possible = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
-
-	for(var i = 0; i < 10; i++)
-	{
-		text += possible.charAt(Math.floor(Math.random() * possible.length));
-	}
-
-	return text;
-}
-
 function startPlayer()
 {
 	/* When on mobile */ 
