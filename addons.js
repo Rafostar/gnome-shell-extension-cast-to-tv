6d44cb7c54c79adce52050364026a88b8a2549df
Rafostar <40623528+Rafostar@users.noreply.github.com>
2019-08-01 13:20:07 +0200

    Move addon enable/disable code to main extension

--- addons.js
+++ addons.js
@@ -1,5 +1,10 @@
+const { GLib } = imports.gi;
 const Main = imports.ui.main;
 const AggregateMenu = Main.panel.statusArea.aggregateMenu;
+
+let castMenu;
+let addonMenuItems = {};
+let timeouts = {};
 
 function findCastToTv()
 {
@@ -23,4 +28,65 @@ function setLastMenuItem(extMenu, item, endOffset)
 	let subMenuItems = extMenu.castSubMenu.menu._getMenuItems();
 	let lastItemIndex = subMenuItems.length - 1;
 	extMenu.castSubMenu.menu.moveMenuItem(item, lastItemIndex - endOffset);
+}
+
+function enableAddon(addonName, Widget, delay)
+{
+	if(!addonName || !Widget) return;
+
+	if(isNaN(delay))
+		delay = 1000;
+
+	if(!timeouts[addonName])
+	{
+		/* Give main extension time to finish startup */
+		timeouts[addonName] = GLib.timeout_add(GLib.PRIORITY_DEFAULT, delay, () =>
+		{
+			delete timeouts[addonName];
+
+			castMenu = findCastToTv();
+			if(castMenu)
+			{
+				addonMenuItems[addonName] = new Widget.addonMenuItem();
+				castMenu.castSubMenu.menu.addMenuItem(addonMenuItems[addonName]);
+
+				if(	typeof castMenu.isServiceEnabled !== 'undefined'
+					&& castMenu.isServiceEnabled === false
+				) {
+					addonMenuItems[addonName].actor.hide();
+				}
+
+				if(typeof castMenu.serviceMenuItem !== 'undefined')
+					setLastMenuItem(castMenu, castMenu.serviceMenuItem);
+
+				if(typeof castMenu.settingsMenuItem !== 'undefined')
+					setLastMenuItem(castMenu, castMenu.settingsMenuItem);
+			}
+
+			return GLib.SOURCE_REMOVE;
+		});
+	}
+}
+
+function disableAddon(addonName)
+{
+	if(timeouts[addonName])
+	{
+		GLib.source_remove(timeouts[addonName]);
+		delete timeouts[addonName];
+	}
+
+	if(addonMenuItems[addonName])
+	{
+		/* No need to reorder menu items when locking screen,
+		as whole cast menu will be destroyed then anyway */
+		let lockingScreen = (Main.sessionMode.currentMode == 'unlock-dialog'
+			|| Main.sessionMode.currentMode == 'lock-screen');
+
+		if(!lockingScreen && castMenu)
+			setLastMenuItem(castMenu, addonMenuItems[addonName]);
+
+		addonMenuItems[addonName].destroy();
+		delete addonMenuItems[addonName];
+	}
 }
