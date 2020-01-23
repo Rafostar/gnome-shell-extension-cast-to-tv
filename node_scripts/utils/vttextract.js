5e74c7430319b154e6ac71cfaf08c868669c34cf
Rafostar <40623528+Rafostar@users.noreply.github.com>
2020-01-23 14:15:26 +0100

    vttextract tool initial version

--- node_scripts/utils/vttextract.js
+++ node_scripts/utils/vttextract.js
@@ -1,12 +1,266 @@
 #!/usr/bin/env node
 
-function extractSubs()
+const fs = require('fs');
+const path = require('path');
+const ffprobe = require('../ffprobe');
+const extractVid = require('../extract/extract-video');
+const gnome = require('../gnome');
+const metadata = require('../../metadata');
+
+const cursor = {
+	show: () => process.stdout.write('\u001B[?25h'),
+	hide: () => process.stdout.write('\u001B[?25l')
+};
+
+var opts = {
+	ffprobePath: null,
+	inPath: null,
+	outDir: null,
+	quiet: false,
+	recursive: false
+};
+
+function showHelp()
 {
+	const version = (metadata.git) ? `git-${metadata.git}` : `v${metadata.version}`;
+
 	console.log([
 		``,
-		`vttextract - Cast to TV subtitles extractor`,
+		`vttextract - Cast to TV ${version} subtitles extractor`,
+		``,
+		`Usage: vttextract <source> [OPTIONS]`,
+		``,
+		`  source - can be path to single video file or dir with videos`,
+		``,
+		`OPTIONS:`,
+		`  -q, --quiet          Do not print extraction info except errors`,
+		`  -r, --recursive      Extract from files in subdirectories`,
 		``
 	].join('\n'));
 }
 
-extractSubs();
+function logInfo(text)
+{
+	if(!opts.quiet)
+		console.log(text);
+}
+
+function writeProgress(fileName, percent)
+{
+	var blackSquare = String.fromCharCode(9632);
+	var whiteSquare = String.fromCharCode(9633);
+	var numSquares = Math.floor(percent / 10);
+	var progress = "";
+
+	while(numSquares--)
+		progress += blackSquare;
+
+	while(progress.length < 10)
+		progress += whiteSquare;
+
+	process.stdout.cursorTo(0);
+	process.stdout.clearLine(0);
+	process.stdout.write(`${fileName} ${progress}`);
+}
+
+function parseArgs()
+{
+	for(var i = 2; i < process.argv.length; i++)
+	{
+		switch(process.argv[i])
+		{
+			case '-q':
+			case '--quiet':
+				opts.quiet = true;
+				break;
+			case '-r':
+			case '--recursive':
+				opts.recursive = true;
+				break;
+			default:
+				if(!opts.inPath)
+					opts.inPath = process.argv[i];
+				else
+					return false;
+				break;
+		}
+	}
+
+	return (opts.inPath !== null);
+}
+
+function extractFromDir(dirPath)
+{
+	logInfo(`Browsing dir: "${dirPath}"`);
+
+	return new Promise((resolve, reject) =>
+	{
+		fs.readdir(dirPath, async (err, files) =>
+		{
+			if(err) return reject(err);
+
+			for(var fileName of files)
+			{
+				var runPath = path.join(dirPath, fileName);
+				await runInPath(runPath, opts.recursive).catch(onReject);
+			}
+
+			resolve();
+		});
+	});
+}
+
+function extractFromFile(filePath)
+{
+	return new Promise((resolve, reject) =>
+	{
+		var ffprobeOpts = {
+			ffprobePath : opts.ffprobePath,
+			filePath: filePath
+		};
+
+		ffprobe(ffprobeOpts, (err, data) =>
+		{
+			if(err)
+			{
+				/* Returns process error when run on non-video file */
+				if(err.message.includes('FFprobe process error'))
+					return resolve();
+				else
+					return reject(err);
+			}
+
+			if(!extractVid.getIsSubsMerged(data))
+				return resolve();
+
+			var parsed = path.parse(filePath);
+
+			/*
+				Access is done here, so we disable second
+				access test with overwrite set to true
+			*/
+			var extOpts = {
+				file: filePath,
+				outPath: path.join(opts.outDir, parsed.name + '.vtt'),
+				overwrite: true
+			};
+
+			fs.access(extOpts.outPath, fs.constants.F_OK, (err) =>
+			{
+				if(!err) return resolve();
+
+				var fileName = parsed.name + parsed.ext;
+
+				if(!opts.quiet)
+				{
+					cursor.hide();
+					writeProgress(fileName, 0);
+				}
+
+				extractVid.videoToVtt(extOpts, (err) =>
+				{
+					if(err)
+					{
+						cursor.show();
+						return reject(err);
+					}
+
+					if(!opts.quiet)
+					{
+						writeProgress(fileName, 100);
+						process.stdout.write('\n');
+						cursor.show();
+					}
+
+					resolve();
+				});
+			});
+		});
+	});
+}
+
+function runInPath(runPath, isRecursive)
+{
+	return new Promise((resolve, reject) =>
+	{
+		fs.access(runPath, fs.constants.F_OK, (err) =>
+		{
+			if(err) return reject(err);
+
+			fs.stat(runPath, async (err, stat) =>
+			{
+				if(err) return reject(err);
+
+				if(stat.isDirectory())
+				{
+					if(isRecursive)
+						await extractFromDir(runPath).catch(onReject);
+				}
+				else if(stat.isFile())
+					await extractFromFile(runPath).catch(onReject);
+				else
+					return reject(new Error(`Not a file or dir: ${runPath}`));
+
+				resolve();
+			});
+		});
+	});
+}
+
+function onReject(err)
+{
+	console.error(err.message);
+}
+
+function onFinish(hideMsg)
+{
+	if(hideMsg)
+		console.log();
+	else
+		logInfo('Extraction finished');
+
+	cursor.show();
+	process.exit(0);
+}
+
+function onUncaughtException(err)
+{
+	/* Stopping ffprobe in middle causes JSON error */
+	if(err && !err.message.includes('JSON'))
+		console.error(err.message);
+
+	cursor.show();
+	process.exit(1);
+}
+
+function startExtract()
+{
+	const argsOk = parseArgs();
+
+	if(!argsOk)
+		return showHelp();
+
+	gnome.loadSchema();
+
+	if(!gnome.getBoolean('extractor-reuse'))
+		return console.error('Extractor is disabled');
+
+	opts.outDir = gnome.getSetting('extractor-dir');
+
+	if(!opts.outDir)
+		return console.error('Could not obtain save dir setting');
+
+	const ffprobePath = gnome.getSetting('ffprobe-path');
+	opts.ffprobePath = ffprobePath || '/usr/bin/ffprobe';
+
+	process.on('SIGINT', () => onFinish(true));
+	process.on('SIGTERM', () => onFinish(true));
+	process.on('uncaughtException', onUncaughtException);
+
+	/* First run must be recursive to support starting in dir */
+	runInPath(opts.inPath, true)
+		.then(onFinish)
+		.catch(onReject)
+}
+
+startExtract();
