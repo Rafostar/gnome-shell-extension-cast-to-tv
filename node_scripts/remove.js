963372e82be0893395540f534a2eaf8f517db7a0
Rafostar <40623528+Rafostar@users.noreply.github.com>
2020-02-03 16:46:28 +0100

    Add file remove debug

--- node_scripts/remove.js
+++ node_scripts/remove.js
@@ -1,4 +1,5 @@
 const fs = require('fs');
+const debug = require('debug')('remove');
 const shared = require('../shared');
 const noop = () => {};
 
@@ -6,14 +7,29 @@ exports.file = function(filePath, cb)
 {
 	cb = cb || noop;
 
+	debug(`Removing ${filePath}...`);
 	fs.access(filePath, fs.constants.F_OK, (err) =>
 	{
-		if(err) return cb(new Error(`File ${filePath} does not exist`));
+		if(err)
+		{
+			var noFileMsg = `File ${filePath} does not exist`;
+			debug(noFileMsg);
+
+			return cb(new Error(noFileMsg));
+		}
 
 		fs.unlink(filePath, (err) =>
 		{
-			if(err) cb(new Error(`Could not remove file: ${filePath}`));
-			else cb(null);
+			if(!err)
+			{
+				debug(`Removed ${filePath}`);
+				return cb(null);
+			}
+
+			var noUnlinkMsg = `Could not remove file: ${filePath}`;
+			debug(noUnlinkMsg);
+
+			return cb(new Error(noUnlinkMsg));
 		});
 	});
 }
